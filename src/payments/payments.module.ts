8c41c0f45b3abbb652504064acd344c2e017606c
Komla Adzam <adzamkomla.dev@gmail.com>
2024-01-18 05:08:46 +0000

    import prisma service

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { PaymentsService } from './payments.service';
+
+import { PrismaService } from '@common/services/prisma.service';
+
 import { PaymentsController } from './payments.controller';
+
+import { PaymentsService } from './payments.service';
 
 @Module({
   controllers: [PaymentsController],
-  providers: [PaymentsService],
+  providers: [PrismaService, PaymentsService],
 })
-export class PaymentsModule {}
+export class PaymentsModule { }
